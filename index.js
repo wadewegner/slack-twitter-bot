0390b249694e09a54a634e077dd57ca7cee8ae87
Wade Wegner <wade.wegner@gmail.com>
2017-07-08 11:09:42 -0700

    Better error logging

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const einsteinIntentHelper = require('./lib/einstein_intent.js');
 const postgresHelper = require('./lib/postgres.js');
 const twitterHelper = require('./lib/twitter.js');
 const slackHelper = require('./lib/slack.js');
+const textFormatterHelper = require('./lib/text_formatting.js');
 
 const the_interval = process.env.LOOPINTERVAL * 60 * 1000;
 const local = process.env.LOCAL;
-
 const bot = slackHelper.getBot();
+
 bot.on('start', () => {
 
-  console.log('Starting bot service ...'); // eslint-disable-line no-console
+  console.log('Starting bot service ...');
   slackHelper.postMessageToUser(bot, 'wadewegner', `Reporting for service (local: ${local})!`);
 
   setInterval(() => {
@@ -28,45 +29,18 @@ bot.on('start', () => {
           const exists = twitterHelper.checkExists(result, id);
 
           if (!exists) {
-            console.log(`Doesn't exist: ${url}`); // eslint-disable-line no-console
+            console.log(`Doesn't exist: ${url}`);
             tweetText = tweetText.replace(/'/g, "''");
             const insertQuery = `INSERT INTO posted_tweets (url, id_str, tweet_text) VALUES ('${url}', '${id}', '${tweetText}')`;
 
             postgresHelper.insertTweet(insertQuery, local).then(() => {
               einsteinTokenHelper.getAccessToken().then((accessToken) => {
-                einsteinSentimentHelper.getSentiment(accessToken, tweetText).then((sentimentResponseBody) => {
-                  einsteinIntentHelper.getIntent(accessToken, tweetText).then((intentResponseBody) => {
+                einsteinSentimentHelper.getSentiment(accessToken, tweetText).then((sentimentBody) => {
+                  einsteinIntentHelper.getIntent(accessToken, tweetText).then((intentBody) => {
 
-                    const sentimentBody = JSON.parse(sentimentResponseBody);
-                    const intentBody = JSON.parse(intentResponseBody);
-                    let sentiment_face = '';
-                    let intent = '';
-                    let insertion = '';
-
-                    if (!sentimentBody.message) {
-                      sentiment_face = einsteinSentimentHelper.getSentimentFace(sentimentBody);
-                    }
-                    if (!intentBody.message) {
-                      intent = einsteinIntentHelper.getIntentCategory(intentBody);
-                    }
-
-                    if (sentiment_face) {
-                      insertion += sentiment_face;
-                    }
-                    if (intent) {
-                      if (sentiment_face) {
-                        insertion += ` ${intent}`;
-                      } else {
-                        insertion += intent;
-                      }
-                    }
-                    if ((sentiment_face !== '') || (intent !== '')) {
-                      insertion += ' ';
-                    }
-                    
-
+                    const insertion = textFormatterHelper.formatEinsteinText(sentimentBody, intentBody);
                     slackHelper.handleMessage(bot, 'salesforcedxeyes', `I found a ${insertion}tweet! ${url}`, local);
-
+                    
                   });
                 });
               });
